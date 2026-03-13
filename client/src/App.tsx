e97c7f09d4b55740fa427fbc2d736791ad052c49
ichinosetakayuki <ichinosetakayuki92@gmail.com>
2026-03-13 21:20:40 +0900

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <button className='bg-blue-500 text-white px-4 py-2 rounded'>Click me</button>
+    </div>
+  )
+}
+
+export default App
