7c1fa09467348a7eb5c4e0ed9282f7cf4ba17dd0
ichinosetakayuki <ichinosetakayuki92@gmail.com>
2026-03-14 16:34:24 +0900

    Shadcn/uiの導入

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,10 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from './assets/vite.svg'
-// import heroImg from './assets/hero.png'
-// import './App.css'
+import { Button } from "./components/ui/button"
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <div>
-      <button className='bg-blue-500 text-white px-4 py-2 rounded'>Click me</button>
+      <Button>Click me</Button>
     </div>
   )
 }
