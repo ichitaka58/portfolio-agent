009fd5286d8ad8e4f1de98996de7e5675ffa4178
ichinosetakayuki <ichinosetakayuki92@gmail.com>
2026-03-16 15:52:05 +0900

    qiitaエージェント&mainエージェントの実装

--- agent/src/index.ts
+++ agent/src/index.ts
@@ -14,69 +14,68 @@ import {
 import { createPinoLogger } from "@voltagent/logger";
 import { honoServer } from "@voltagent/server-hono";
 import { expenseApprovalWorkflow } from "./workflows";
-import { weatherTool } from "./tools";
-import z from "zod";
+import { weatherTool, getQiitaUserInfo, getQiitaUserItems } from "./tools";
 
 // Create a logger instance
-const logger = createPinoLogger({
-  name: "agent",
-  level: "info",
-});
+// const logger = createPinoLogger({
+//   name: "agent",
+//   level: "info",
+// });
 
 // Configure persistent memory (LibSQL / SQLite)
-const memory = new Memory({
-  storage: new LibSQLMemoryAdapter({
-    url: "file:./.voltagent/memory.db",
-    logger: logger.child({ component: "libsql" }),
-  }),
-});
+// const memory = new Memory({
+//   storage: new LibSQLMemoryAdapter({
+//     url: "file:./.voltagent/memory.db",
+//     logger: logger.child({ component: "libsql" }),
+//   }),
+// });
 
 // Configure persistent observability (LibSQL / SQLite)
-const observability = new VoltAgentObservability({
-  storage: new LibSQLObservabilityAdapter({
-    url: "file:./.voltagent/observability.db",
-  }),
+// const observability = new VoltAgentObservability({
+//   storage: new LibSQLObservabilityAdapter({
+//     url: "file:./.voltagent/observability.db",
+//   }),
+// });
+
+
+
+const qiitaAgent = new Agent({
+  name: "qiita-agent",
+  instructions:
+    "ユーザーからQiitaユーザーIDを受け取ったら、ユーザーの情報と投稿記事一覧を取得してください。",
+  model: "google/gemini-2.5-flash",
+  tools: [getQiitaUserInfo, getQiitaUserItems],
 });
 
-const agent = new Agent({
-  name: "agent",
+const mainAgent = new Agent({
+  name: "main-agant",
   instructions:
-    "A helpful assistant that can check weather and help with various tasks",
-  model: "google/gemini-2.0-flash",
-  tools: [weatherTool],
-  memory,
+    "ユーザーから「QiitaユーザーID」を受け取ったら、「QiitaユーザーID」を渡して情報をJSONで取得し、Qiitaのユーザー情報と投稿記事一覧をまとめて返してください。",
+  subAgents: [qiitaAgent],
+  model: "google/gemini-2.5-flash",
 });
+
+// const agent = new Agent({
+//   name: "agent",
+//   instructions:
+//     "A helpful assistant that can check weather and help with various tasks",
+//   model: "google/gemini-2.0-flash",
+//   tools: [weatherTool],
+//   memory,
+// });
 
 new VoltAgent({
   agents: {
-    agent,
+    mainAgent,
   },
-  workflows: {
-    expenseApprovalWorkflow,
-  },
+  // workflows: {
+  //   expenseApprovalWorkflow,
+  // },
   server: honoServer(),
-  logger,
-  observability,
-  voltOpsClient: new VoltOpsClient({
-    publicKey: process.env.VOLTAGENT_PUBLIC_KEY || "",
-    secretKey: process.env.VOLTAGENT_SECRET_KEY || "",
-  }),
-});
-
-const getQiitaUserInfo = createTool({
-  name: "getQiitaUserInfo",
-  description: "Qiitaユーザーの情報を取得する",
-  parameters: z.object({
-    userId: z.string().describe("QiitaユーザーID"),
-  }),
-  execute: async ({ userId }) => {
-    const accessToken = process.env.QIITA_API_KEY;
-    const response = await fetch(`https://qiita.com/api/v2/users/${userId}`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    const data = await response.json();
-    return data;
-  },
+  // logger,
+  // observability,
+  // voltOpsClient: new VoltOpsClient({
+  //   publicKey: process.env.VOLTAGENT_PUBLIC_KEY || "",
+  //   secretKey: process.env.VOLTAGENT_SECRET_KEY || "",
+  // }),
 });
